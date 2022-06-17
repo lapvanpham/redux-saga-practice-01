0732b2b68a303fc101c6a1e05f84e09cc27f409e
Lap Van Pham <caramel-apple@outlook.com>
2022-06-17 13:52:30 +0700

    done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import './App.css';
-import Login from './middlewares/redux-saga/components/Login';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import User from "./components/User";
 
 function App() {
   return (
-    <div className="App">
-      <Login />
-    </div>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/users" element={<User />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   );
 }
-
 export default App;
--- src/middlewares/redux-saga/redux/store.js
+++ src/middlewares/redux-saga/redux/store.js
@@ -1,9 +1,15 @@
-import {createSagaMiddleware} from 'redux-saga';
-import {applyMiddleware, createStore} from 'redux';
-import {rootReducer} from './reducer';
+import { createStore, applyMiddleware } from "redux";
+import rootReducer from "./reducer";
+import createSagaMiddleware from "redux-saga";
+import rootSaga from "../saga/userSaga";
 
+// Apply middleware
 const sagaMiddleware = createSagaMiddleware();
+
+// Đăng kí reducer cho redux quản lí
 const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
 
+// Chạy middleware cho redux để chạy các effect tại dòng code
 sagaMiddleware.run(rootSaga);
+
 export default store;
